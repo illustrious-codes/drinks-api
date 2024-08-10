db6215557f1ddb7323e19b94f539b9a973b7403d
illustrious-codes <onatayodavid105@gmail.com>
2024-08-10 01:31:25 +0100

    drinks

---
+++ drinks/serializers.py
@@ -0,0 +1,7 @@
+from .models import Drink
+from rest_framework import serializers
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id', 'name', 'description']
